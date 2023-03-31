9ae5e767d77dda294d486a27cac080cff94e5384
Erick D <eridanpersan@gmail.com>
2023-03-31 19:59:35 -0300

    31/03 19:59

--- app/Models/Administrador.php
+++ app/Models/Administrador.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Aluno extends Model
+class Admnistrador extends Model
 {
     use HasFactory;
 
-    protected $table = 'alunos';
+    protected $table = 'administrador';
 }
