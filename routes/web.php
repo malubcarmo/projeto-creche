263c17a2ed4af391c65908455e2922cc31a9dba9
ErickDPS <eridanpersan@gmail.com>
2023-04-03 19:29:25 -0300

    Att

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
